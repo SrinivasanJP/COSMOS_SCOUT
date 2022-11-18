fd6ec5d72af6e052109cf1c7c321374a9f8fa914
SrinivasanJP <jpsrinivasan38@gmail.com>
2022-11-18 21:28:34 +0530

    Initial commit

---
+++ app/src/main/java/com/srini/cosmos_scout/MainActivity.java
@@ -0,0 +1,46 @@
+package com.srini.cosmos_scout;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private ProgressBar vProgressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        vProgressBar = findViewById(R.id.progress);
+        vProgressBar.setVisibility(View.VISIBLE);
+        vProgressBar.setMax(100);
+        webView.loadUrl("https://zoom.earth/");
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) vProgressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+            }
+
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
+            }
+        });
+    }
+}
